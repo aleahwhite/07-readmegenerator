e2e254276ec6780791ec4ebe11557f82d7314656
Aleah White <aleahwhite55@gmail.com>
2024-01-21 23:34:52 -0600

    major index.js changes, slight changes to other files

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
 // TODO: Include packages needed for this application
 
+const { default: inquirer } = require("inquirer");
+const fs = require('fs');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+   {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+   },
+   {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a shprt descri[tins of your project!'
+   },
+   {
+    type: 'input',
+    name: 'usage',
+    message: 'How is your project used?'
+   },
+   {
+    type: 'input',
+    name: 'credits',
+    message: '(optional) List any collaborators used, any 3rd party assets used or any tutorials followed.'
+   },
+   {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username.'
+   },
+   {
+    type: 'input',
+    name: 'email',
+    message: 'Please enter your email!'
+   },
+   {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project! Or not.',
+    choices: ['MIT License', 'Apache 2.0', 'GPL 3.0', 'Mozilla Public 2.0', 'None']
+   }
+];
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        if (err) {
+            console.error('Error writing file: ', err);
+        } else {
+            console.log('README.md has been generated!');
+        }
+    });
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function(answers){
+        const data = JSON.stringify(answers, null, ' ');
+        writeToFile('README.md', data);
+    }).catch(function(error){
+        console.error('An error occurred: ');
+    });
+}
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,64 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT License':
+      return
+    case 'Apache 2.0':
+      return
+    case 'GPL 3.0':
+      return
+    case 'Mozilla Public 2.0':
+      return
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT License':
+      return
+    case 'Apache 2.0':
+      return
+    case 'GPL 3.0':
+      return
+    case 'Mozilla Public 2.0':
+      return
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+  ${data.description}
+  
+## Table of Contents 
+-[Usage](#usage)
+-[Credits](#credits)
+-[Questions](#questions)
+-[License](#license)
+
+## Usage
+${data.usage}
+
+## Credits
+${data.credits}
+
+## Questions
+if you have any questions/comments about the project, you can email me at [${data.email}](mailto:${data.email}). For more of my work you can visit my GitHub at [${data.github}](https://github.com/${data.github}).
+
+## License
+${renderLicenseSection(data.license)}
 
 `;
 }
